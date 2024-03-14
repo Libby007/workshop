eada9ba68d80e361dd315c0c73af4797728153f2
Libby007 <107486104+Libby007@users.noreply.github.com>
2024-03-14 11:30:08 -0400

    Add files via upload

---
+++ sql.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+with st.sidebar:
+    choose = option_menu("DS Project",
+                         ["About", 
+                          "Toronto condo price prediction", 
+                          "Condo clustering", "Condo search engine"
+                             , "Multi-criteria ranked condos", "Talk to AI Libby", "Algorithm theory and principle"],
+                         icons=['fingerprint', 'house fill', 'kanban', 'google', 'award', 'robot', 'book'],
+                         menu_icon="app-indicator", default_index=0,
+                         styles={
+                             "container": {"padding": "5!important", "background-color": "#fafafa"},
+                             "icon": {"color": "orange", "font-size": "18px"},
+                             "nav-link": {"font-size": "14px", "text-align": "left", "margin": "0px",
+                                          "--hover-color": "#eee"},
+                             "nav-link-selected": {"background-color": "#02ab21"},
+                         }
+                         )
